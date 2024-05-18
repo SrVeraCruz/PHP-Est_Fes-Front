01a527247a8429e61802da1c04d148b363cdaaa2
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-05-18 02:53:32 +0100

    Fixing system pattern

---
+++ includes/footer.php
@@ -0,0 +1,64 @@
+  </main>
+  <!-- Main end -->
+
+  <!-- Footer start -->
+  <footer>
+    <div class="boxContainer">
+      <div class="content">
+
+        <div class="box">
+          <div class="boxImg">
+            <img src="./assets/img/est_logo_mobile3.png" alt="est_logo">
+          </div>
+          <p>Ecole Supérieure de Technologie Fès</p>
+          <p>BP: 2427</p>
+          <p>Route d'Imouzzer</p>
+          <p>30000</p>
+          <p>Fès - Maroc</p>
+          <p>Campus Universitaire</p>
+          <p>Tel : +212 5 35 60 05 84</p>
+          <p>Fax : +212 5 35 60 05 88</p>
+          <a href="mailto:support.est@usmba.ac.ma">E-mail : support.est@usmba.ac.ma</a>
+        </div>
+
+        <div class="box">
+          <h3>Liens rapides</h3>
+          <ul>
+            <li><a href="./filiere_DUT.php">Diplôme Universitaire de Technologies</a></li>
+            <li><a href="./filiere_LP.php">Licence Profissionnelle</a></li>
+            <li><a href="./fomation_cont_DU.php">Formations Continues</a></li>
+            <li><a href="./esp_et_ENT.php">Espace Numerique De Travail</a></li>
+          </ul>
+        </div>
+
+        <div class="box">
+          <h3>Outres liens</h3>
+          <ul>
+            <li><a href="./404.php">nous facebook</a></li>
+            <li><a href="./404.php">nous instagram</a></li>
+            <li><a href="./404.php">actualité</a></li>
+            <li><a href="./404.php">newsletter</a></li>
+          </ul>
+        </div>
+
+        <div class="box">
+          <h3>Suive nous</h3>
+          <ul>
+            <li><a href="./404.php">facebook</a></li>
+            <li><a href="./404.php">instagram</a></li>
+            <li><a href="./404.php">twitter</a></li>
+            <li><a href="./404.php">youTube</a></li>
+          </ul>
+        </div>
+
+      </div>
+      <div class="content">
+        <p>Université Sidi Mohamed Ben Abdellah - Ecole Superieure de Technologie Fès</p>
+        <p>&copy; Copyright 2023 all rights reserved | <span>Vera Cruz Dúdú</span></p>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer end -->
+  </body>
+
+  </html>
